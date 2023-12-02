2db1ee2bf1a2e690ca9f57b9f16fbfd50527648f
MattD78 <109258394+MattD78@users.noreply.github.com>
2023-12-02 01:59:20 -0500

    more condensing and improving readability

--- quicksort.h
+++ quicksort.h
@@ -7,26 +7,32 @@ int partition(std::vector<Movie> &movie_list, int low, int high, std::string fil
 void quickSort(std::vector<Movie> &movie_list, int low, int high, std::string filter, bool descending);
 
 int partition(std::vector<Movie> &movie_list, int low, int high, std::string filter, bool descending) {
-	//setting pivot point, can sort by id, popularity, budget, revenue, runtime, vote average, or vote count
 	double pivot;
+	//setting pivot point, can sort by id, popularity, budget, revenue, runtime, vote average, or vote count
 	if (filter == "id" || filter == "ID" || filter == "Id") {
 		pivot = movie_list[low].movieID;
 	}
+
 	else if (filter == "budget" || filter == "Budget") {
 		pivot = movie_list[low].movieBudget;
 	}
+
 	else if (filter == "revenue" || filter == "Revenue") {
 		pivot = movie_list[low].movieRevenue;
 	}
+
 	else if (filter == "runtime" || filter == "Runtime") {
 		pivot = movie_list[low].movieRuntime;
 	}
+
 	else if (filter == "vote average" || filter == "Vote Average" || filter == "Vote average") {
 		pivot = movie_list[low].voteAverage;
 	}
+
 	else if (filter == "vote count" || filter == "Vote Count" || filter == "Vote count") {
 		pivot = movie_list[low].voteCount;
 	}
+
 	//popularity as default
 	else {
 		pivot = movie_list[low].popularityScore;
@@ -41,54 +47,49 @@ int partition(std::vector<Movie> &movie_list, int low, int high, std::string fil
 		for (int i = up; i < high; i++) {
 			//for each search filter: id, popularity, budget, revenue, runtime, vote average, and vote count
 			if (filter == "id" || filter == "ID" || filter == "Id") {
-				//sort by descending order or ascending order
+				//sort by descending order or ascending order, respectively
 				if ((descending && movie_list[up].movieID < pivot) || (!descending && movie_list[up].movieID > pivot)) {
 					break;
 				}
-				up++;
 			}
 
 			else if (filter == "budget" || filter == "Budget") {
 				if ((descending && movie_list[up].movieBudget < pivot) || (!descending && movie_list[up].movieBudget > pivot)) {
 					break;
 				}
-				up++;
 			}
 
 			else if (filter == "revenue" || filter == "Revenue") {
 				if ((descending && movie_list[up].movieRevenue < pivot) || (!descending && movie_list[up].movieRevenue > pivot)) {
 					break;
 				}
-				up++;
 			}
 
 			else if (filter == "runtime" || filter == "Runtime") {
 				if ((descending && movie_list[up].movieRuntime < pivot) || (!descending && movie_list[up].movieRuntime > pivot)) {
 					break;
 				}
-				up++;
 			}
 
 			else if (filter == "vote average" || filter == "Vote Average" || filter == "Vote average") {
 				if ((descending && movie_list[up].voteAverage < pivot) || (!descending && movie_list[up].voteAverage > pivot)) {
 					break;
 				}
-				up++;
 			}
 
 			else if (filter == "vote count" || filter == "Vote Count" || filter == "Vote count") {
 				if ((descending && movie_list[up].voteCount < pivot) || (!descending && movie_list[up].voteCount > pivot)) {
 					break;
 				}
-				up++;
 			}
 
 			else {
 				if ((descending && movie_list[up].popularityScore < pivot) || (!descending && movie_list[up].popularityScore > pivot)) {
 					break;
-				}
-				up++;
+				}	
 			}
+			
+			up++;
 		}
 
 		//decrement until down less than pivot
@@ -97,50 +98,45 @@ int partition(std::vector<Movie> &movie_list, int low, int high, std::string fil
 				if ((descending && movie_list[down].movieID > pivot) || (!descending && movie_list[down].movieID < pivot)) {
 					break;
 				}
-				down--;
 			}
 
 			else if (filter == "budget" || filter == "Budget") {
 				if ((descending && movie_list[down].movieBudget > pivot) || (!descending && movie_list[down].movieBudget < pivot)) {
 					break;
 				}
-				down--;
 			}
 
 			else if (filter == "revenue" || filter == "Revenue") {
 				if ((descending && movie_list[down].movieRevenue > pivot) || (!descending && movie_list[down].movieRevenue < pivot)) {
 					break;
 				}
-				down--;
 			}
 
 			else if (filter == "runtime" || filter == "Runtime") {
 				if ((descending && movie_list[down].movieRuntime > pivot) || (!descending && movie_list[down].movieRuntime < pivot)) {
 					break;
 				}
-				down--;
 			}
 
 			else if (filter == "vote average" || filter == "Vote Average" || filter == "Vote average") {
 				if ((descending && movie_list[down].voteAverage > pivot) || (!descending && movie_list[down].voteAverage < pivot)) {
 					break;
 				}
-				down--;
 			}
 
 			else if (filter == "vote count" || filter == "Vote Count" || filter == "Vote count") {
 				if ((descending && movie_list[down].voteCount > pivot) || (!descending && movie_list[down].voteCount < pivot)) {
 					break;
 				}
-				down--;
 			}
 
 			else {
 				if ((descending && movie_list[down].popularityScore > pivot) || (!descending && movie_list[down].popularityScore < pivot)) {
 					break;
 				}
-				down--;
 			}
+
+			down--;
 		}
 
 		//swap up and down
